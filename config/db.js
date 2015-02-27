0f24479d99e49c4d9f05370c100e3861628cd063
Johannes Weber <johannes.weber@mayflower.de>
2015-02-27 15:19:07 +0100

    added database name to config file
    in order to be able to connect to a database

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
 	module.exports = {
-		url : '' //Input DB URL here, localhost:27017 if running mongodb locally
+		url : 'mongodb://localhost/insecure-dev' //Input DB URL here, localhost:27017 if running mongodb locally
 	}
 
